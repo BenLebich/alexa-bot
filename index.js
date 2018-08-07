72441fceaa63a0030ec0e60a2513eff13bdd4b4a
Ben <lebich11benny@gmail.com>
2018-08-06 17:45:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ bot.on("message", async message => {
     .addField("Bot info", "!botinfo")
     .addField("Report User", "!report @user some reason")
     .addField("Random Quote", "!getquote");
-
+    console.log("Listing Commands");
     return message.channel.send(serverembed);
   }
 
